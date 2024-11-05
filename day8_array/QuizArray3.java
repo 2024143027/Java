30e590ecf9be4f77fab9e69a73111ba4ae85c435
2024143027 <hyeongmin0325@g.shingu.ac.kr>
2024-11-05 12:21:02 +0900

    Add files via upload

---
+++ day8_array/QuizArray3.java
@@ -0,0 +1,11 @@
+package day8_array;
+
+public class QuizArray3 {
+
+	public static void main(java.lang.String[] args) {
+		String[] strArr = {"자바","완전","쉬워","어려워","좋아","싫어"};
+		System.out.printf("%s %s %s\n", strArr[0], strArr[1], strArr[3]);
+
+	}
+
+}
